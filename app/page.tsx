0855ae1c36681414724f8e997036cdfafff2ce74
David Temitope <Temi236topemicheal@gmail.com>
2023-10-14 04:05:47 +0100

    image change

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
     <main className="bg-gradient-to-r from-[#fac61b] from-10% via-[#f39f20] via-30% to-[#f38120] to-90% h-[50vh]">
       <div className="w-full h-[30%] bg-gradient-to-r from-[#fac61b] from-10% via-[#f39f20] via-30% to-[#f38120] to-90% ">
         <div className="flex flex-col gap-y-2 lg:w-[50%] h-full mx-auto text-center items-center justify-center pt-20">
-          <p className="uppercase text-base text-white mb-2">
+          <p className="uppercase text-base text-white mb-2 font-semibold">
             Lorem ipsum dolor sit{" "}
           </p>
           <p className=" text-3xl lg:text-5xl font-semibold text-white capitalize mb-4">
             inspiring excellence
           </p>
-          <p className="text-white">
+          <p className="text-white font-semibold">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
             accusamus quibusdam, assumenda iusto velit earum voluptas cupiditate
             officiis fuga dignissimos{" "}
