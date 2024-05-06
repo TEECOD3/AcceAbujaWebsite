59f2e12437096e39b4a6bc7b442b8bdd11ad6501
David Temitope <Temi236topemicheal@gmail.com>
2024-05-06 18:46:09 +0100

    feat(TeamsApi):integrating teams Api endpoints

--- components/Home/Our-Team.tsx
+++ components/Home/Our-Team.tsx
@@ -5,7 +5,7 @@ import getTeams from "@/lib/getTeam";
 type Props = {};
 
 const OurTeam = async (props: Props) => {
-  // const Teamdata = await getTeams();
+  const Teamdata = await getTeams();
 
   return (
     <section className="  bg-[#FFF7F0] flex items-center justify-center flex-col gap-8 lg:gap-y-20  lg:px-0 py-10 lg:py-20">
@@ -15,15 +15,28 @@ const OurTeam = async (props: Props) => {
         </h1>
       </div>
       <div className="w-full md:w-[65%] lg:w-[80%] mx-auto  grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 x1place-items-center gap-y-6 gap-x-20">
-        <OurTeamItem />
-        <OurTeamItem />
-        <OurTeamItem />
-        <OurTeamItem />
+        {Teamdata?.data.slice(0, 4).map(
+            (team: {
+              id: number;
+              name: string;
+              title: string;
+              iamge: string;
+            }) => (
+              <OurTeamItem key={team.name} data={team} />
+            )
+          )}
       </div>
-      <div className="w-full lg:w-[80%] mx-auto  grid grid-cols-1 max-md:grid-rows-3 sm:grid-cols-2 md:grid-cols-3 place-items-center gap-4">
-        <OurTeamItem />
-        <OurTeamItem />
-        <OurTeamItem />
+      <div className="w-full lg:w-[80%] mx-auto  grid grid-cols-1 max-md:grid-rows-2 sm:grid-cols-2 md:grid-cols-2 place-items-center gap-4">
+        {Teamdata?.data.slice(4, 7).map(
+            (team: {
+              id: number;
+              name: string;
+              title: string;
+              iamge: string;
+            }) => (
+              <OurTeamItem key={team.name} data={team} />
+            )
+          )}
       </div>
     </section>
   );
--- components/ui/OurTeam-Item.tsx
+++ components/ui/OurTeam-Item.tsx
@@ -3,25 +3,33 @@ import React from "react";
 import president from "../../public/president.jpg";
 
 type Props = {
-  image?: StaticImageData;
+  data: {
+    id: number;
+    name: string;
+    title: string;
+    iamge: string;
+  };
 };
 
 const OurTeamItem = (props: Props) => {
+  const {
+    data: { iamge, name, title },
+  } = props;
   return (
     <>
       <div className="flex flex-col items-center">
         <figure className=" h-[250px]  w-[250px] relative md:h-[270px] md:w-[270px] overflow-hidden flex items-center justify-center rounded-full">
           <Image
-            src={president}
-            alt="watermark logo"
+            src={iamge || president}
+            alt="teamimage"
             fill
             className="h-full w-full object-cover object-top"
           />
         </figure>
 
         <figcaption className="flex flex-col gap-y-2 mt-4 items-center ">
-          <p className="font-semibold text-xl capitalize">Dr.M. K.Dikwa,mni</p>
-          <p>founder</p>
+          <p className="font-semibold text-xl capitalize">{name}</p>
+          <p>{title}</p>
         </figcaption>
       </div>
     </>
