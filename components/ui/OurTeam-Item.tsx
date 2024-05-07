b05df4a591b54ac380baf28355d1afb7296f17cc
David Temitope <Temi236topemicheal@gmail.com>
2024-05-07 12:22:10 +0100

    refactor(Teams):adding default text and images for teams section

--- components/ui/OurTeam-Item.tsx
+++ components/ui/OurTeam-Item.tsx
@@ -20,7 +20,7 @@ const OurTeamItem = (props: Props) => {
       <div className="flex flex-col items-center">
         <figure className=" h-[250px]  w-[250px] relative md:h-[270px] md:w-[270px] overflow-hidden flex items-center justify-center rounded-full">
           <Image
-            src={iamge || president}
+            src={president}
             alt="teamimage"
             fill
             className="h-full w-full object-cover object-top"
@@ -28,8 +28,8 @@ const OurTeamItem = (props: Props) => {
         </figure>
 
         <figcaption className="flex flex-col gap-y-2 mt-4 items-center ">
-          <p className="font-semibold text-xl capitalize">{name}</p>
-          <p>{title}</p>
+          <p className="font-semibold text-xl capitalize">Team member name</p>
+          <p>Position</p>
         </figcaption>
       </div>
     </>
