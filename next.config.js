104a09669e9074c25a2f602d3b10292879cf2b77
Temitope David <temi236topemicheal@gmail.com>
2024-07-11 12:54:10 +0100

    fix: adjusting redirects

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "host",
-            value: "www.acce-abuja.com",
-          },
-        ],
-        destination: "https://acce-abuja.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       has: [
+  //         {
+  //           type: "header",
+  //           key: "host",
+  //           value: "www.acce-abuja.com",
+  //         },
+  //       ],
+  //       destination: "https://acce-abuja.com/:path*",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
   images: {
     domains: ["dashboard.acce-abuja.com"],
     remotePatterns: [{ hostname: "cdn.sanity.io" }],
