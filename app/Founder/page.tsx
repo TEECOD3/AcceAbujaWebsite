d65f58931c8552419b3df4be1df80fb0370a5cf7
Temitope David <temi236topemicheal@gmail.com>
2024-07-23 13:08:42 +0100

    style: change founder image

--- app/Founder/page.tsx
+++ app/Founder/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import founderImage from "../../public/founderlatest.jpeg";
 import logo from "../../public/image2.png";
 import ClientButton from "@/components/ui/ClientButton";
+import MarkdownRenderer from "@/components/markdownrenderer";
 
 type Props = {};
 
@@ -55,72 +56,7 @@ const Founderpage = (props: Props) => {
           </p>
         </div>
 
-        <p>
-          I am tremendously proud to lead a dedicated and hardworking team. Our
-          priority is our pupils and our society. My team and I will strive to
-          provide the highest possible standard of education at all times for
-          your child, Insha Allah. We discovered a lack of understanding of
-          religion and its moral teachings in the Islamic youth today and we are
-          poised to solve this problem. I am highly confident of the team we
-          have on ground, their experience and dedication. I ask Allah to be our
-          support and to help us achieve far beyond what you will be expecting
-          as parents. At ACCE, we plan to equip our pupils with essential
-          knowledge and skills for living a pious life in a 21st century world.
-          Our energy is directed towards building in our pupils the confidence
-          to study, understand and apply any subject they have learnt in real
-          life situations, so that they graduate well-informed and can
-          successfully and confidently follow whatever career path they choose
-          in life. We will equally equip pupils with the knowledge of Deen;
-          Quran memorization, Arabic, Adhkar, Tauheed, Fiqhu, Hadeeth, in a way
-          they will have a good understanding of and apply to their daily life.
-          Our main aim is to ensure that all our pupils study in a safe, secure
-          and happy environment at the school and graduate appreciating their
-          high level of achievement.
-        </p>
-        <p>
-          We discovered a lack of understanding of religion and its moral
-          teachings in the Islamic youth today and we are poised to solve this
-          problem. I am highly confident of the team we have on ground, their
-          experience and dedication. I ask Allah to be our support and to help
-          us achieve far beyond what you will be expecting as parents. At ACCE,
-          we plan to equip our pupils with essential knowledge and skills for
-          living a pious life in a 21st century world. Our energy is directed
-          towards building in our pupils the confidence to study, understand and
-          apply any subject they have learnt in real life situations, so that
-          they graduate well-informed and can successfully and confidently
-          follow whatever career path they choose in life.
-        </p>
-        <p>
-          We are also proud of our curriculum; It is broad, balanced and
-          relevant to the needs of all our pupils. At the primary education
-          stage, we deliver a formal broad education system, focusing on
-          literacy and numeracy. We also introduce our pupils to a well-designed
-          and comprehensive curriculum of Islamic Religious Knowledge and
-          Information, Communication Technology (ICT)
-        </p>
-        <p>
-          In our secondary school, we have in addition to our curriculum, a
-          skills acquisition content. We believe the 21st century pupil deserves
-          an all round experience and learning should extend beyond the
-          classroom. These include the Al-Ansar Leadership Programme (ALP),
-          Young Journalists, Qur’an Memorisation Masters, Train the Trainers
-          Club (TTC), Young Farmers Club, STEM Club, Young Writers Club, The
-          Duke of Edinburgh’s Award Scheme as well as numerous community based
-          projects. These programmes open our pupils’ minds to new experiences,
-          provide opportunities for them to work with professionals in their
-          various fields of expertise and to ultimately develop leadership
-          skills and qualities which are valued by universities, employers and
-          the society
-        </p>
-
-        <p>
-          We have zero tolerance for any form of mischief, such as exam
-          malpractice, drug abuse, bullying etc. Lastly, our carefully selected
-          team of Imams offer Islamic pastoral care to our pupils in order to
-          help strengthen their Imaan and knowledge of the Deen in line with the
-          teachings of the Qur’an and Sunnah. We look forward to receiving you
-          and your child in ACCE, Abuja
-        </p>
+        <MarkdownRenderer filePath="/md-files/foundermessage.md" />
 
         <div className="flex flex-wrap items-center">
           <span>
