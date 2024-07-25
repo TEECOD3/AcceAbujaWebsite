29913c6c3d4b505fbe86e526f633f2a7ed22a32a
Temitope David <temi236topemicheal@gmail.com>
2024-07-25 02:31:26 +0100

    feat: adding a 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,16 +7,16 @@ type Props = {};
 
 const NotFoundPage = (props: Props) => {
   return (
-    <div className="min-h-screen text-orange h-screen w-screen flex items-center justify-center text-center">
+    <div className="min-h-screen text-orange h-screen w-screen flex  items-center justify-center text-center flex-col gap-y-4">
       <div className="">
-        <span className=" text-xl lg:text-3xl text-bold">
+        <span className=" text-xl lg:text-3xl font-bold capitalize animate-pulse">
           Requested page not Found
         </span>
         <Link
           href="/"
           className={cn(
             buttonVariants({ variant: "default" }),
-            "bg-orange-600 text-white"
+            "bg-orange-600 text-white capitalize font-semibold"
           )}
         >
           Go back to home page
