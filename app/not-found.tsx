034d0ccdfd5cfafc0d84988a2605c131be823dae
Temitope David <temi236topemicheal@gmail.com>
2024-07-25 02:24:08 +0100

    feat: adding a 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const NotFoundPage = (props: Props) => {
+  return (
+    <div className="min-h-screen text-orange h-screen w-screen flex items-center justify-center text-center">
+      <div className="">
+        <span className=" text-xl lg:text-3xl">Requested page not Found</span>
+        <Link
+          href="/"
+          className={cn(buttonVariants({ variant: "default" }), "bg-orange")}
+        >
+          Go back to home page
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
